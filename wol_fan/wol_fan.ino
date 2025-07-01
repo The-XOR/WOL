05d08f88213f5946c298643e7b20940d1ec93015
The XOR <1h3x0r@gmail.com>
2025-07-01 14:00:05 +0200

    wol fan

---
+++ wol_fan/wol_fan.ino
@@ -0,0 +1,160 @@
+// Device: VALTRACK_V4_MFW_ESP32_C3
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define SERIAL_PRINT
+#define THEXOR // OCIO! comment out this line if you != me
+#ifdef THEXOR
+  #include "/home/io/Vaults/Segretoni/include/wifi.h"
+#else
+  const char *ssid = "TODO: SSID";  // <<<<<<<<<<<<<<<<<<<<<<<<<
+  const char *password = "TODO: PASSWORD"; // <<<<<<<<<<<<<<<<<<<<<<<<<
+  const char *dnsProbe = "TODO: WIFI CONNECTIVITY CHECK"; // <<<<<<<<<<<<<<<<<<<<<<
+#endif
+
+const int outputPin = 3; // Pin connesso al mosfet/rele/ecc
+int curStatus = 0; // 0 = OFF, 1 = ON
+
+WebServer server(80);
+
+void sendStatus()
+{
+  if(curStatus == 0)
+    server.send(200, "text/html", "0");
+  else
+    server.send(200, "text/html", "1");
+}
+
+void handleRoot() 
+{
+  if(curStatus == 0)
+    server.send(200, "text/html", "<h1>FAN status: OFF</h1><p><a href=\"/on\">Turn On</a></p>");
+  else
+    server.send(200, "text/html", "<h1>FAN status: ON</h1><p><a href=\"/off\">Turn Off</a></p>");
+}
+
+
+void handleStatus()
+{
+  sendStatus();
+}
+
+void spegni()
+{
+    #ifdef SERIAL_PRINT
+      Serial.println("___ spengo ___");
+    #endif
+
+    digitalWrite(outputPin, LOW);
+    curStatus = 0;
+}
+
+void accendi()
+{
+    #ifdef SERIAL_PRINT
+      Serial.println("___ accendo ___");
+    #endif
+
+    digitalWrite(outputPin, HIGH);
+    curStatus = 1;
+}
+
+void handleOn() 
+{
+  if(curStatus == 0)
+    accendi();
+  
+  sendStatus();
+}
+
+void handleOff() {
+  if(curStatus > 0)
+    spegni();
+
+  sendStatus();
+}
+
+void check_connection()
+{
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    server.close();
+
+    #ifdef SERIAL_PRINT
+      Serial.println("**** CONNECTION LOST ****");
+    #endif
+
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    #ifdef SERIAL_PRINT
+      Serial.println("Reconnecting to WiFi...");
+    #endif
+  for(int k=0; k<10;k++)
+    {
+      if(WiFi.status() == WL_CONNECTED) 
+        break;
+      delay(500);
+      #ifdef SERIAL_PRINT
+        Serial.print(".");
+      #endif
+    }
+
+    if(WiFi.status() == WL_CONNECTED) 
+      startWebServer();
+  }
+}
+
+void setup() 
+{
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+  curStatus = 0; // Inizializza lo stato a OFF
+
+  #ifdef SERIAL_PRINT
+    Serial.begin(115200);
+    Serial.println("WOL is starting");
+    Serial.println("Connecting to WiFi...");
+  #endif
+
+  WiFi.begin(ssid, password);
+  for(int k=0; k<10;k++)
+  {
+    if(WiFi.status() == WL_CONNECTED) 
+      break;
+    delay(500);
+    #ifdef SERIAL_PRINT
+      Serial.print(".");
+    #endif
+  }
+
+  if(WiFi.status() == WL_CONNECTED) 
+  {
+    #ifdef SERIAL_PRINT
+      Serial.println(" connected!");
+      Serial.print("IP Address: ");
+      Serial.println(WiFi.localIP());
+    #endif
+
+    startWebServer();
+  }
+}
+
+void startWebServer()
+{
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.on("/status", handleStatus);
+
+  server.begin();
+  #ifdef SERIAL_PRINT
+    Serial.println("HTTP server started");
+  #endif
+}
+
+void loop() 
+{
+  server.handleClient();
+  check_connection();
+  delay(500);
+}
